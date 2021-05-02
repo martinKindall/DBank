ce44580a36de105beac1397dc7836117936a301c
martinKindall <mart256@gmail.com>
2021-05-02 15:05:06 +0200

    Tests: avoid using call(). DBank, starting deposit method

--- truffle/contracts/DBank.sol
+++ truffle/contracts/DBank.sol
@@ -6,7 +6,20 @@ import "./Token.sol";
 contract DBank {
     Token private token;
 
+     mapping(address => bool) public isDeposited;
+     mapping(address => uint) public etherBalanceOf;
+     mapping(address => uint) public depositStart;
+
     constructor (Token _token) {
         token = _token;
+    }
+    
+    function deposit() payable public {
+        require(isDeposited[msg.sender] == false, 'Error, deposit already active');
+        require(msg.value >= 1e16, 'Error, deposit must be >= 0.01 ETH');
+
+        etherBalanceOf[msg.sender] = msg.value;
+        isDeposited[msg.sender] = true;
+        depositStart[msg.sender] = block.timestamp;
     }
 }
--- truffle/test/TokenTest.js
+++ truffle/test/TokenTest.js
@@ -5,13 +5,13 @@ contract('Token and DBank', ([deployer, user]) => {
 
   it('Passing mint role properly at new Token instance', async() => {
     const token = await MyToken.new();
-    const address = await token.minter.call();
+    const address = await token.minter();
     assert.equal(address, deployer, 'Minter and Deployed mismatch');
   });
 
   it('Checking DBank has minting Role', async() => {
     const token = await MyToken.deployed();
-    const minterAddress = await token.minter.call();
+    const minterAddress = await token.minter();
     const dBank = await DBank.deployed();
 
     assert.equal(dBank.address, minterAddress, "DBank has no Mint Role");
