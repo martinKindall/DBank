7daa6e239860fec13fffb7e83934d387d6b5404d
martinKindall <mart256@gmail.com>
2021-05-02 15:37:11 +0200

    TokenTest: added negative test for mint role passing.

--- truffle/test/TokenTest.js
+++ truffle/test/TokenTest.js
@@ -9,6 +9,19 @@ contract('Token and DBank', ([deployer, user]) => {
     assert.equal(address, deployer, 'Minter and Deployed mismatch');
   });
 
+  it('Rejecting changing Mint Role from wrong user', async() => {
+    const token = await MyToken.deployed();
+    try {
+      await token.passMinterRole(user, {from: user});
+    } catch (err) {
+      assert.equal(
+        err.reason, 
+        'Error, only the minter can pass the minter role.',
+        'Rejecting Mint Role not throwing correct msg'
+        );
+    }
+  });
+
   it('Checking DBank has minting Role', async() => {
     const token = await MyToken.deployed();
     const minterAddress = await token.minter();
